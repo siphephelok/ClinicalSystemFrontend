4549042aa3805fe9391882f09f79f2f57f6233f5
SiphepheloMyeni <217014127@stu.ukzn.ac.za>
2024-06-14 17:07:54 +0200

    front

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FileUploadComponent } from './file-upload/file-upload.component';
-import { FileListComponent } from './file-list/file-list.component';
-import { FileDetailsComponent } from './file-details/file-details.component';
-import { FileDeleteComponent } from './file-delete/file-delete.component';
-import { FileDownloadComponent } from './file-download/file-download.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
@@ -12,13 +7,26 @@ import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { StudentDashboardComponent } from './student-dashboard/student-dashboard.component';
+import { FileListComponent } from './file-list/file-list.component';
+import { FileUploadComponent } from './file-upload/file-upload.component';
+import { FileDownloadComponent } from './file-download/file-download.component';
+import { FileDeleteComponent } from './file-delete/file-delete.component';
+import { FileDetailsComponent } from './file-details/file-details.component';
+
 const routes: Routes = [
-  { path: 'upload', component: FileUploadComponent },
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'admin-dashboard', component: AdminDashboardComponent },
+  { path: 'student-dashboard', component: StudentDashboardComponent },
   { path: 'files', component: FileListComponent },
-  { path: 'files/:name', component: FileDetailsComponent },
-  { path: 'delete', component: FileDeleteComponent },
+  { path: 'upload', component: FileUploadComponent },
   { path: 'download', component: FileDownloadComponent },
-  { path: '', redirectTo: '/files', pathMatch: 'full' }
+  { path: 'delete', component: FileDeleteComponent },
+  { path: 'details', component: FileDetailsComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
 @NgModule({
